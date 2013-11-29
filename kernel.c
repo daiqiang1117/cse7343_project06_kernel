2b8a95105d2c6e50143b2fdd217cd59a3f20ae36
Andy Qiang <daiqiang1117@hotmail.com>
2013-11-28 23:05:00 -0600

    change the naive solution and add the script

--- kernel.c
+++ kernel.c
@@ -3,31 +3,22 @@
 	
 	Qiang Dai
 */
-	
+
+/* display this string on the screen */
+char str[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '\0'};
+
+int segment = 0xB000;
+int base_address = 0x8000;
+int color_white = 0x07;
+
 int main()
 {
-	putInMemory(0xB000, 0x8000, 'H');
-	putInMemory(0xB000, 0x8001, 0x7);
-	putInMemory(0xB000, 0x8002, 'e');
-	putInMemory(0xB000, 0x8003, 0x7);
-	putInMemory(0xB000, 0x8004, 'l');
-	putInMemory(0xB000, 0x8005, 0x7);
-	putInMemory(0xB000, 0x8006, 'l');
-	putInMemory(0xB000, 0x8007, 0x7);
-	putInMemory(0xB000, 0x8008, 'o');
-	putInMemory(0xB000, 0x8009, 0x7);
-	putInMemory(0xB000, 0x800A, ' ');
-	putInMemory(0xB000, 0x800B, 0x7);
-	putInMemory(0xB000, 0x800C, 'W');
-	putInMemory(0xB000, 0x800D, 0x7);
-	putInMemory(0xB000, 0x800E, 'o');
-	putInMemory(0xB000, 0x800F, 0x7);
-	putInMemory(0xB000, 0x8010, 'r');
-	putInMemory(0xB000, 0x8011, 0x7);
-	putInMemory(0xB000, 0x8012, 'l');
-	putInMemory(0xB000, 0x8013, 0x7);
-	putInMemory(0xB000, 0x8014, 'd');
-	putInMemory(0xB000, 0x8015, 0x7);
+	int i;
+	for(i = 0; str[i] != '\0'; i++)
+	{
+		putInMemory(segment, base_address + i, str[i]);
+		putInMemory(segment, base_address + i + 1, color_white);
+	}
 
 	while(1)
 	{
